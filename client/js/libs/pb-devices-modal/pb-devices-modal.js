62cfa31185f2216b010c60d0204c9d064d74559e
jychuah <jychuah@gmail.com>
2016-02-02 16:16:05 -0600

    Devices modal manually loads listgroup.js

--- client/js/libs/pb-devices-modal/pb-devices-modal.js
+++ client/js/libs/pb-devices-modal/pb-devices-modal.js
@@ -2,16 +2,24 @@ define(['jquery', 'require', 'module', 'bootstrap'], function($, require, module
   PBDevicesModal = function(particleBaseInstance) {
     var pb = particleBaseInstance;
     this.$pbdevicesmodal = $('.pb-devices-modal');
-    var htmlPath = require.toUrl(module.id);
-    htmlPath = htmlPath.substring(0, htmlPath.lastIndexOf('/')) + '/html/pb-devices-modal.html';
-    $('.pb-devices-modal').load(htmlPath, null, this.init);
+    var path = require.toUrl(module.id);
+    path = path.substring(0, path.lastIndexOf('/'));
+    var htmlPath = path + '/html/pb-devices-modal.html';
+
+    function init() {
+      console.log("PB Devices Modal Hello World");
+      $.getScript(path + '/listgroup.min.js');
+    }
+
+    $('.pb-devices-modal').addClass('modal fade');
+    $('.pb-devices-modal').prop('role', 'dialog');
+    $('.pb-devices-modal').load(htmlPath, null, init);
+
+
   }
 
   PBDevicesModal.prototype = {
     constructor: PBDevicesModal,
-    init : function() {
-      console.log("PB Devices Modal Hello World");
-    }
   }
 
   return PBDevicesModal;
